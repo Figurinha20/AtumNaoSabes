91826d836934748ce2d69fbd9641817de82c225c
Your_Majesty <9180284@esmad.ipp.pt>
2019-04-23 12:26:41 +0100

    new code plan for quiz removed while()

--- js/quiz.js
+++ js/quiz.js
@@ -8,14 +8,17 @@ import {newQuestion} from "./models/newQuestion";
 //eventlistener selecionar radio button conta como resposta
 
 function jogar(){
-    //nivel 
+
+
+    //nivel & game functionalities
     let level = 1;
     let difficulty = 1;
+    let difficultyCount = 1;
     let lifes = 3;
     let winingBool= true;
- 
-    //enquanto não errar função não pára
-    while(winingBool == true){
+
+    //reward
+    let exp = 0;
  
     //buscar question;;
     
@@ -23,20 +26,35 @@ function jogar(){
 
     injectQuestion(currentQuestion, level)
     
-    //if para modificar a pagina mediante o type de question
+    //if para modificar a pagina mediante o type de question, e event listeners respetivos
  
-    //await event resposta
-    await
-
     
-    //if resposta correta: (level++; exp += 2; else winingBool = false
- 
+    
+    if("resposta correta"){
+       level++
+       difficultyCount++
+
+       //sobe dificuldade de 3 a 3 perguntas
+       if(difficultyCount%3 === 0 && difficulty != 10){
+          difficulty++
+       }
+
+       exp += 5*level*lifes;
+
+       //Waow!; reset de botoes;  
+
+    }else{
+       winingBool=false
+       //mudar screen para winning screen
+    }
+    
  
     }
-    //recompensa por chegar longe exExp += level - (type of question respetivamente ExHard Hard Medium Easy => 2  8  12  20)
-    //se exExp<0 exExp = 0;
+
+    //atribuir recompensa
+    console.log(`You gained ${exp} exp!`)
  
- }
+ 
 
  function questionSelector(difficulty){
 
