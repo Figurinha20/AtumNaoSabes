441a21ec94554dc688f31b6e543aa639716946bf
ZeBitzz <9180270@esmad.ipp.pt>
2019-05-28 09:41:09 +0100

    Avanços no registo/login

--- js/controllers/signUp.js
+++ js/controllers/signUp.js
@@ -8,54 +8,57 @@ let users = []
 const myForm = document.querySelector("form")
 myForm.addEventListener("submit", function (event) {
 
-    let formValid = true;
+
     //Receber dados
     let newUsername = document.querySelector("#txtUsername").value
     let newPassword = document.querySelector("#txtPassword").value
     let newConfirmPassword = document.querySelector("#txtConfirmPassword").value
-console.log(newUsername)
-console.log(newPassword)
-console.log(newConfirmPassword)
-   ///Verificar se as duas passwords são iguais
-    if (newPassword != newConfirmPassword) {
-        alert("As passwords não são iguais")
-       formValid = false
-    }
+    console.log(newUsername)
+    console.log(newPassword)
+    console.log(newConfirmPassword)
 
-    
 
     //Verificar se Utilizador Já existe
-    
-    if (isUser(newUsername) == true){
-        alert("Utilizador já existente")
-    }
-    else{
-        const new_user = new newUser(newUsername, newPassword)
-    }
-    
-    //push para array
-    users.push(new newUser)
 
-    //armazenamento na base de dados
+    if (isUser(newUsername) == true) {
+        alert("Utilizador já existente")
+        return;
+    } else {
+
+        ///Verificar se as duas passwords são iguais
+        if (newPassword != newConfirmPassword) {
+            alert("As passwords não são iguais")
+            return;
+        }
+
+        //push para array
+        users.push(new newUser(newUsername, newPassword))
+        
+        //armazenamento na base de dados
+        localStorage.setItem("users", JSON.stringify(users))
+    }
+
+
+
 
     alert("Sucesso! Bem vindo " + newUsername + "!")
- 
+
     //prevenir que form seja submetido
     event.preventDefault()
 
-    alert(users)
+    console.log(users)
 
-    return formValid;
+    return;
 })
 
 
 
 //Função para verificar se o utilizador já existe
- function isUser(newUsername) {
+function isUser(newUsername) {
     for (const user of users) {
         if (newUser.username === newUsername) {
             return true;
         }
     }
-    return false;   
-}   
+    return false;
+}
