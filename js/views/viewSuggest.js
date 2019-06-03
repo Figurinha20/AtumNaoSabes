6aa045f00bdd2234cabdb18029c45583b567c644
Figurinha20 <figurinhapt@gmail.com>
2019-06-03 23:14:04 +0100

    Fiz JS do Suggest! Falta criar "suggestions" na LS

---
+++ js/views/viewSuggest.js
@@ -0,0 +1,33 @@
+//Import the Suggestion Model
+import {Suggestion} from "../models/Suggestion";
+
+
+
+
+const myForm = document.querySelector("#formSuggest")
+myForm.addEventListener("submit", function (event) {
+
+    //Import the Current User from Local Storage
+    let currentUser = localStorage.getItem("currentUser")
+
+    //Receber dados
+    let newMessage = document.querySelector("#txtSuggest").value
+
+
+    //Buscar array à localStorage
+    let suggestions = JSON.parse(localStorage.getItem("suggestions"));
+
+
+    //push para array
+    suggestions.push(new Suggestion(currentUser, newMessage))
+
+    //armazenamento na base de dados
+    localStorage.setItem("suggestions", JSON.stringify(suggestions))
+
+
+    alert("Sugestão Recebida! Obrigado" + currentUser + "!")
+
+    //prevenir que form seja submetido
+    event.preventDefault()
+
+})
