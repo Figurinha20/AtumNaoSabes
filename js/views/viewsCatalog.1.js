765c26d2e5eb473bda1f4f9f2b8d8e49c29e84d7
Your_Majesty <9180284@esmad.ipp.pt>
2019-06-05 16:04:29 +0100

    eu tentei

--- js/views/viewsCatalog.1.js
+++ js/views/viewsCatalog.1.js
@@ -1,5 +1,4 @@
 
-import {renderCatalog} from "../controllers/controlsCatalog.js"
 
 import {Card} from "../models/Card.js"
 //on load = show all
@@ -17,7 +16,7 @@ renderCatalog()
 
 
 
-function renderCatalogo(filterName = "") {
+function renderCatalog(filterName = "") {
     let cards = JSON.parse(localStorage.getItem("cards"))
     const myCatalog = document.querySelector("#divForCards")
     let result = ""
@@ -29,75 +28,52 @@ function renderCatalogo(filterName = "") {
         }
         // Criação de linha
         if (i % 3 === 0) {
-            result += `<div class="row">`
+            result += `<div class="row">
+                        <div class="col-sm-1">
+
+                        </div>
+
+                        <div class="col-sm-10">
+                            <div class="row">`
         }
         // Geração do card
         result += `
 
 
-<div class="col-sm-1">
 
-</div>
-
-<div class="col-sm-10">
-    <div class="row">
         <div class="col-sm-4">
             <div id="card" class="card border-dark mb-3" style="width: 18rem;">
-                <h5 id="cardName" class="card-title text-center font-weight-bold">Nome do Peixe</h5>
+                <h5 id="cardName" class="card-title text-center font-weight-bold">${cards[i].name}</h5>
                 <img id="cardImg" class="card-img-top"
-                    src="https://oceana.org/sites/default/files/styles/ntsc/public/magma_wrasse3_b.p._shutman.jpg?itok=pKC8AHK"
-                    alt="Card image cap">
+                    src="${cards[i].img}"
+                    alt="${cards[i].name}">
                 <div class="card-body">
-                    <p id="cardText" class="card-text">[Filtros]</p>
+                    <p id="cardText" class="card-text">[${cards[i].tags}]</p>
                     <hr>
                     <a id="cardBtn" href="#" class="btn btn-dark">Ver Mais</a>
                 </div>
             </div>
         </div>
 
-        <div class="col-sm-4">
-            <div id="card" class="card border-dark mb-3" style="width: 18rem;">
-                <h5 id="cardName" class="card-title text-center font-weight-bold">Nome do Peixe</h5>
-                <img id="cardImg" class="card-img-top"
-                    src="https://oceana.org/sites/default/files/styles/ntsc/public/magma_wrasse3_b.p._shutman.jpg?itok=pKC8AHK"
-                    alt="Card image cap">
-                <div class="card-body">
-                    <p id="cardText" class="card-text">[Filtros]</p>
-                    <hr>
-                    <a id="cardBtn" href="#" class="btn btn-dark">Ver Mais</a>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-sm-4">
-            <div id="card" class="card border-dark mb-3" style="width: 18rem;">
-                <h5 id="cardName" class="card-title text-center font-weight-bold">Nome do Peixe</h5>
-                <img id="cardImg" class="card-img-top"
-                    src="https://oceana.org/sites/default/files/styles/ntsc/public/magma_wrasse3_b.p._shutman.jpg?itok=pKC8AHK"
-                    alt="Card image cap">
-                <div class="card-body">
-                    <p id="cardText" class="card-text">[Filtros]</p>
-                    <hr>
-                    <a id="cardBtn" href="#" class="btn btn-dark">Ver Mais</a>
-                </div>
-            </div>
-        </div>
-
-
-    </div>
-</div>
-
-<div class="col-sm-1">
-
-</div>
+        
 
 
 
 `
         // Criação do fecho da linha
         if (i % 3 === 0) {
-            result += `</div>`
+            result += `
+                        </div>
+                    </div>
+        
+                    <div class="col-sm-1">
+        
+                    </div>
+        
+            </div>`
         }
+        
+        i++
     }
 
     // Atribuição de todos os cards gerados ao elemento com id myCatalog
