510fb3a53e44ea4a2848f81612aac38f8099e213
Your_Majesty <9180284@esmad.ipp.pt>
2019-06-07 15:05:17 +0100

    commit filtros e search a funcionar //o zé é um inutil

--- js/views/viewsCatalog.1.js
+++ js/views/viewsCatalog.1.js
@@ -6,11 +6,18 @@ renderCatalog("","")
 
 
 //onseacrh ou onclick = refresh
-document.querySelector("#txtSearch").addEventListener("search", function (event){
+document.querySelector("#formSearch").addEventListener("submit", function (event){
     console.log("search")
-    let search = document.querySelector("#txtSearch").value
+    let search = document.querySelector("#txtSearch").value.toLowerCase()
     
+    console.log(search)
+     
+event.preventDefault()
+
+
 renderCatalog("", search)
+
+
 })
 
 
