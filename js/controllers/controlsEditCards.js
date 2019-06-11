d58855c799657b98befa9dcacd875c52125f8108
Your_Majesty <9180284@esmad.ipp.pt>
2019-06-11 12:07:07 +0100

    purging "tag" replacing for "category"

--- js/controllers/controlsEditCards.js
+++ js/controllers/controlsEditCards.js
@@ -16,13 +16,13 @@ export function makeArray(string){
     return array;
 }
 
-export function replaceCard(cardName, newImage, newTags, newDescription, newLinks){
+export function replaceCard(cardName, newImage, newCategory, newDescription, newLinks){
     let cards = JSON.parse(localStorage.getItem("cards"))
     for (const card of cards) {
         if (card.name === cardName) {
             card.description = newDescription
             card.img = newImage
-            card.tags = newTags
+            card.category = newCategory
             card.etc = newLinks
         }
     }
