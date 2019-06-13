381aec744439132b984c67b90574528aa9432b12
Figurinha20 <figurinhapt@gmail.com>
2019-06-13 15:06:27 +0100

    Tentativa de pôr suggestões a funcionar

--- js/views/viewSuggestAdmin.js
+++ js/views/viewSuggestAdmin.js
@@ -6,6 +6,32 @@ let suggestions = JSON.parse(localStorage.getItem("suggestions"))
 
 renderSuggestions(suggestions);
 
+/* Esta bosta não dá
+function clickStar(){
+    const btnStars = document.getElementsByClassName("btnStar")
+    console.log(btnStars);
+    console.log("desisto")
+    
+    for (const elem of btnStars) {
+        console.log(elem)
+        elem.addEventListener("click", function () {
+            approved(this.id)
+            console.log("ola")
+        })
+
+    }
+
+}
+
+function approved(messages){
+    for (const suggestion of suggestions) {
+        if (suggestion.message == messages) {
+            alert(messages)
+        }
+    }
+}
+´*/
+
 
 //Renderizar Sugestões
 function renderSuggestions(suggestions){
@@ -40,7 +66,7 @@ function renderSuggestions(suggestions){
         <div class="col-sm-7">
         </div>
         <div class="col-sm-1">
-        <input id="btnStar${counter}" type="image" src="${approvalStar}" height="35px" width="35px">
+        <input class="btnStar" id="${suggestion.message}" type="image" src="${approvalStar}" height="35px" width="35px">
         <p>${suggestion.date}</p>
         </div>
         
@@ -48,10 +74,16 @@ function renderSuggestions(suggestions){
 
     `
 
+    
     counter ++
     }
 
+    //clickStar()
+
     mySuggestions.innerHTML = result;
 }
+
+
+
 
 
