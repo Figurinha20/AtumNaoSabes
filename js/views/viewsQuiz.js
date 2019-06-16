8102638769947cd3b0ee262a59ac7a611d296356
Your_Majesty <9180284@esmad.ipp.pt>
2019-06-16 23:24:07 +0100

    hints no quiz implementadas

--- js/views/viewsQuiz.js
+++ js/views/viewsQuiz.js
@@ -1,4 +1,9 @@
-import {renderLifes, injectQuestionTypeComplete, injectQuestionTypeMultiple, questionSelector} from "../controllers/controlQuiz.js"
+import {
+   renderLifes,
+   injectQuestionTypeComplete,
+   injectQuestionTypeMultiple,
+   questionSelector
+} from "../controllers/controlQuiz.js"
 
 let currentQuestion;
 
@@ -6,7 +11,6 @@ let currentQuestion;
 let questions = JSON.parse(localStorage.getItem("questions"))
 
 //valores para jogar
-
 let stage
 let lifes
 let exp
@@ -20,39 +24,41 @@ const difficultyLimit = 10
 
 //set dos eventListeners nos radio buttons e no text
 for (let i = 1; i < 5; i++) {
-    
-    //em cada opção a mesma função é chamada
-    document.querySelector(`#opt${i}`).addEventListener("click",  answerMultiple)
- }
 
-document.querySelector("#formAnswerComplete").addEventListener("submit",  answerComplete)
+   //em cada opção a mesma função é chamada
+   document.querySelector(`#opt${i}`).addEventListener("click", answerMultiple)
+}
 
+document.querySelector("#formAnswerComplete").addEventListener("submit", answerComplete)
 
-
-
-
-
- gameStart()
+//listeners para os butoes hint
+document.querySelector("#btnHint1").addEventListener("click", showHint)
+document.querySelector("#btnHint2").addEventListener("click", showHint)
 
 
 
 
+gameStart()
 
 
 
 
-function gameStart(){
-  
 
-    //nivel & game functionalities set para o começo do quiz
-    stage = 1;
-    lifes = 3;
-  
 
-    //declarar recompensa
-    exp = 0;
 
-    gameProgress()
+
+function gameStart() {
+
+
+   //nivel & game functionalities set para o começo do quiz
+   stage = 1;
+   lifes = 3;
+
+
+   //declarar recompensa
+   exp = 0;
+
+   gameProgress()
 }
 
 
@@ -64,31 +70,31 @@ function gameStart(){
 
 
 //Seguir para a pergunta seguinte
-function gameProgress(){
+function gameProgress() {
 
    console.log("exp = " + exp)
-   
-    resetButtons()
 
-    //selecionar pergunta aleatoria com dificuldade adequada ao stage do quiz
+   resetButtons()
+
+   //selecionar pergunta aleatoria com dificuldade adequada ao stage do quiz
    currentQuestion = questionSelector(questions, stage)
 
 
-    if(currentQuestion.type == "multiple"){
-        //hide complete
-        document.querySelector("#containerForComplete").className = "hidden"
-        //show multiple
-        document.querySelector("#containerForMultiple").className = "container-fluid"
+   if (currentQuestion.type == "multiple") {
+      //hide complete
+      document.querySelector("#containerForComplete").className = "hidden"
+      //show multiple
+      document.querySelector("#containerForMultiple").className = "container-fluid"
 
-        injectQuestionTypeMultiple(currentQuestion, stage, maxDifEz, maxDifMed, maxDifHrd)
-    }else{
-        //show complete
-        document.querySelector("#containerForComplete").className = "container-fluid center"
-        //hide multiple
-        document.querySelector("#containerForMultiple").className = "hidden"
+      injectQuestionTypeMultiple(currentQuestion, stage, maxDifEz, maxDifMed, maxDifHrd)
+   } else {
+      //show complete
+      document.querySelector("#containerForComplete").className = "container-fluid center"
+      //hide multiple
+      document.querySelector("#containerForMultiple").className = "hidden"
 
-        injectQuestionTypeComplete(currentQuestion, stage, maxDifEz, maxDifMed, maxDifHrd)
-    }
+      injectQuestionTypeComplete(currentQuestion, stage, maxDifEz, maxDifMed, maxDifHrd)
+   }
 
 
 }
@@ -99,14 +105,15 @@ function gameProgress(){
 
 
 
-function resetButtons(){
+function resetButtons() {
    //reset buttons
    document.querySelector("#opt1").checked = false
    document.querySelector("#opt2").checked = false
    document.querySelector("#opt3").checked = false
    document.querySelector("#opt4").checked = false
    document.querySelector("#txtAnswer").value = ""
-   document.querySelectorAll("#btnHint").value = "Comprar pista com experiência"
+   document.querySelector("#btnHint1").value = "Comprar pista com experiencia?"
+   document.querySelector("#btnHint2").value = "Comprar pista com experiencia?"
 }
 
 
@@ -119,105 +126,106 @@ function resetButtons(){
 
 
 
-function answerMultiple(event){
-
-    
-      //buscar opção escolhida
-      let answer
-      if(document.querySelector("#opt1").checked == true){
-         answer = document.querySelector("#A").value
-      }else if(document.querySelector("#opt2").checked == true){
-         answer = document.querySelector("#B").value
-      }else if(document.querySelector("#opt3").checked == true){
-         answer = document.querySelector("#C").value
-      }else if(document.querySelector("#opt4").checked == true){
-         answer = document.querySelector("#D").value
-      }
-      
-      
-      //testar se resposta correta
-      if(currentQuestion.answer == answer){
-
-        alert("Resposta Correta!")
-         //avançar no quiz
-         stage++
-        
-         //acrescentar na recompensa final
-         exp += stage+lifes;
-   
-        //caso passe do nivel da dificuldade maxima ganha senão repete a jogada
-        if(stage > difficultyLimit){
-         //VITORIA
-         console.log("VITORIA");
-         
-        }else{
-         gameProgress()
-        }
-   
-      }else{
-         alert("Resposta errada :(")
-         
-         //-1 vida
-         lifes--
-         renderLifes(lifes)
-
-         //GAME OVER PASSAR PARA A RECOMPENSA
-         if(lifes == 0){
-            console.log("GAMEOVER");
-         }
-
-         resetButtons()
-         
-      }
-     
-   
-      
-      
-}
+function answerMultiple(event) {
 
 
-
-
-
-function answerComplete(event){
-
-   
    //buscar opção escolhida
-   let answer = document.querySelector("#txtAnswer").value.toLowerCase()
-  
-   
-   
-   //testar se resposta correta
-   if(currentQuestion.answer.toLowerCase() == answer){
+   let answer
+   if (document.querySelector("#opt1").checked == true) {
+      answer = document.querySelector("#A").value
+   } else if (document.querySelector("#opt2").checked == true) {
+      answer = document.querySelector("#B").value
+   } else if (document.querySelector("#opt3").checked == true) {
+      answer = document.querySelector("#C").value
+   } else if (document.querySelector("#opt4").checked == true) {
+      answer = document.querySelector("#D").value
+   }
 
-     alert("Resposta Correta!")
+
+   //testar se resposta correta
+   if (currentQuestion.answer == answer) {
+
+      alert("Resposta Correta!")
+      //acrescentar na recompensa final
+      exp += stage + lifes;
+
+
       //avançar no quiz
       stage++
-     
-      //acrescentar na recompensa final
-      exp += stage+lifes;
-
 
       //caso passe do nivel da dificuldade maxima ganha senão repete a jogada
-      if(stage > difficultyLimit){
+      if (stage > difficultyLimit) {
          //VITORIA
          console.log("VITORIA");
-           
-      }else{
+
+      } else {
          gameProgress()
       }
-     
 
-   }else{
-
+   } else {
       alert("Resposta errada :(")
-      
+
       //-1 vida
       lifes--
       renderLifes(lifes)
 
       //GAME OVER PASSAR PARA A RECOMPENSA
-      if(lifes == 0){
+      if (lifes == 0) {
+         console.log("GAMEOVER");
+      }
+
+      resetButtons()
+
+   }
+
+
+
+
+}
+
+
+
+
+
+function answerComplete(event) {
+
+
+   //buscar opção escolhida
+   let answer = document.querySelector("#txtAnswer").value.toLowerCase()
+
+
+
+   //testar se resposta correta
+   if (currentQuestion.answer.toLowerCase() == answer) {
+
+      alert("Resposta Correta!")
+      //acrescentar na recompensa final
+      exp += stage + lifes;
+
+      //avançar no quiz
+      stage++
+
+
+      //caso passe do nivel da dificuldade maxima ganha senão repete a jogada
+      if (stage > difficultyLimit) {
+         //VITORIA
+         console.log("VITORIA");
+
+      } else {
+         gameProgress()
+      }
+
+
+   } else {
+
+      alert("Resposta errada :(")
+
+      //-1 vida
+      lifes--
+      renderLifes(lifes)
+
+      //GAME OVER PASSAR PARA A RECOMPENSA
+      if (lifes == 0) {
          console.log("GAMEOVER");
       }
 
@@ -225,6 +233,27 @@ function answerComplete(event){
    }
 
    event.preventDefault()
-   
+
 }
 
+
+
+
+
+
+function showHint() {
+
+   alert("work?")
+
+   let btnHint = document.querySelector("#btnHint1").value
+   console.log(btnHint)
+
+   if (btnHint == "Comprar pista com experiencia?") {
+      alert("please?")
+      document.querySelector("#btnHint1").value = currentQuestion.hint
+      document.querySelector("#btnHint2").value = currentQuestion.hint
+
+      //retirar um pouco da recompensa
+      exp -= 2
+   }
+}
