b1682257518fde3825c94824afeeb28d3b06abe7
Figurinha20 <figurinhapt@gmail.com>
2019-06-17 22:47:22 +0100

    Suggestion now has a disapproval feature!

--- js/views/viewSuggestAdmin.js
+++ js/views/viewSuggestAdmin.js
@@ -10,22 +10,32 @@ renderSuggestions(suggestions);
 //Renderizar Sugestões
 function renderSuggestions(suggestions){
     let approvalStar = ""
+    let disapprovalDownvote = ""
     const mySuggestions = document.querySelector("#divForSuggestions") 
     let result = ""
     let counter = 0
 
-    //Escolher qual estrela usar
+    
     for(const suggestion of suggestions){
 
-        // get imagem do utilizador
+        
         let img =  getUserImg(suggestion.username)
 
+        //Find out which Star Image and Downvote image to use
         if (suggestion.approval == true){
             approvalStar="../img/Star Colored.png"
         }
         else{
             approvalStar="../img/Star.png"
         }
+
+        if (suggestion.disapproval == true){
+            disapprovalDownvote = "../img/Downvote Colored.png"
+        }
+        else{
+            disapprovalDownvote = "../img/Downvote.png"
+        }
+        
         
         result +=       `<div class="row">
         <div class="col-sm-1">
@@ -37,11 +47,11 @@ function renderSuggestions(suggestions){
             <h6 >${suggestion.username}</h6>
             <p>${suggestion.message}</p>
         </div>
-        <div class="col-sm-7">
+        <div class="col-sm-6">
         </div>
-        <div class="col-sm-1">
+        <div class="col-sm-2">
         <input class="btnStar" id="${suggestion.message}" type="image" src="${approvalStar}" height="35px" width="35px">
-        <input class="btnStar" id="downvote-${suggestion.message}" type="image" src="${approvalStar}" height="35px" width="35px">
+        <input class="btnStar" id="downvote-${suggestion.message}" type="image" src="${disapprovalDownvote}" height="35px" width="35px">
         <p>${suggestion.date}</p>
         </div>
             
@@ -59,7 +69,7 @@ function renderSuggestions(suggestions){
     mySuggestions.innerHTML = result;
 }
 
-
+//Event Listeners to approve a suggestion
 for(const suggestion of suggestions){
     document.getElementById(suggestion.message).addEventListener("click", function (){
 
@@ -68,8 +78,10 @@ for(const suggestion of suggestions){
         for(const suggestion of suggestions){
             if (suggestion.message == suggestionToApprove){
                 suggestion.approval = true
+                suggestion.disapproval = false
 
                 document.getElementById(suggestion.message).src = "../img/Star Colored.png"
+                document.getElementById("downvote-" + suggestion.message).src = "../img/Downvote.png"
             }
         }
 
@@ -79,18 +91,24 @@ for(const suggestion of suggestions){
 })
 }
 
+//Event Listeners to disapprove a suggestion
+for(const suggestion of suggestions){
+    document.getElementById("downvote-" + suggestion.message).addEventListener("click", function (){
 
-function approval(suggestionNumber){
+        let suggestionToApprove = suggestion.message
+    
+        for(const suggestion of suggestions){
+            if (suggestion.message == suggestionToApprove){
+                suggestion.approval = false
+                suggestion.disapproval = true
 
-    alert("oi")
-
-    let suggestionToApprove = document.getElementById(suggestionNumber).innerHTML
-
-    console.log(suggestionToApprove)
-
-    for(const suggestion of suggestions){
-        if (suggestion.message == suggestionToApprove){
-            suggestion.approval = true
+                document.getElementById(suggestion.message).src = "../img/Star.png"
+                document.getElementById("downvote-" + suggestion.message).src = "../img/Downvote Colored.png"
+            }
         }
-    }
+
+        localStorage.setItem("suggestions", JSON.stringify(suggestions))
+
+        
+})
 }
