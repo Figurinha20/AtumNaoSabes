762fc76a8a9d83d32b010af4d9f486d2daf4a89f
Your_Majesty <9180284@esmad.ipp.pt>
2019-06-17 12:31:07 +0100

    correções de css + cardView começo do javascript

---
+++ js/views/viewCardDisplay.js
@@ -0,0 +1,15 @@
+
+let displayedCard = JSON.parse(localStorage.getItem("displayedCard"))
+
+let displayedDescription = document.querySelector("#displayedCardDiscription").innerHTML
+let displayedMedia = document.querySelector("#displayedCardMedia").innerHTML
+let commentContainer = document.querySelector("#commentContainer").innerHTML
+
+
+let cardName = document.querySelector("#cardName").innerHTML
+let displayedCategory = document.querySelector("#cardText").innerHTML
+let cardImg = document.querySelector("#cardImg").src
+let cardRank = document.querySelector("#cardRank").innerHTML
+let cardComments = document.querySelector("#cardComments").innerHTML
+
+let likeBtn= document.querySelector("#likeBtn")
