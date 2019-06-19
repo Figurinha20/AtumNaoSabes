6914c9474c9282bc689c059555387de3f5e5393b
Your_Majesty <9180284@esmad.ipp.pt>
2019-06-19 15:14:09 +0100

    "corta-atalhos" implementado

--- js/views/viewSuggestAdmin.js
+++ js/views/viewSuggestAdmin.js
@@ -1,19 +1,28 @@
 //Import the Suggestion Model
-import {getUserImg} from "../controllers/controlSuggestions.js"
-import {levelManager} from "../controllers/controlQuiz.js"
+import {
+    getUserImg
+} from "../controllers/controlSuggestions.js"
+import {
+    levelManager
+} from "../controllers/controlQuiz.js"
 import {
     getUserData
 } from "../controllers/controlsNavbar.js"
 
-//corta atalhos
-let currentUser = sessionStorage.getItem("currentUser")
-let userDataArray
-if(currentUser!= null){
-userDataArray=getUserData(currentUser)
-if(!getUserData[0]){location.href = "../html/index.html"}
-}else{
-    location.href = "../html/index.html"
-}
+//"corta-atalhos"
+window.addEventListener("load", function (event) {
+    let currentUser = sessionStorage.getItem("currentUser")
+    let userDataArray
+    if (currentUser != null) {
+        userDataArray = getUserData(currentUser)
+        if (getUserData[0] == false) {
+            location.href = "../html/index.html"
+        }
+    } else {
+        location.href = "../html/index.html"
+    }
+})
+
 
 
 
@@ -29,36 +38,34 @@ let suggestionReward = 50
 
 
 //Renderizar Sugestões
-function renderSuggestions(suggestions){
+function renderSuggestions(suggestions) {
     let approvalStar = ""
     let disapprovalDownvote = ""
-    const mySuggestions = document.querySelector("#divForSuggestions") 
+    const mySuggestions = document.querySelector("#divForSuggestions")
     let result = ""
     let suggestionReward = 50
 
-    
-    for(const suggestion of suggestions){
 
-        
-        let img =  getUserImg(suggestion.username)
+    for (const suggestion of suggestions) {
+
+
+        let img = getUserImg(suggestion.username)
 
         //Find out which Star Image and Downvote image to use
-        if (suggestion.approval == true){
-            approvalStar="../img/Star Colored.png"
-        }
-        else{
-            approvalStar="../img/Star.png"
+        if (suggestion.approval == true) {
+            approvalStar = "../img/Star Colored.png"
+        } else {
+            approvalStar = "../img/Star.png"
         }
 
-        if (suggestion.disapproval == true){
+        if (suggestion.disapproval == true) {
             disapprovalDownvote = "../img/Downvote Colored.png"
-        }
-        else{
+        } else {
             disapprovalDownvote = "../img/Downvote.png"
         }
-        
-        
-        result +=       `<div class="row">
+
+
+        result += `<div class="row">
         <div class="col-sm-1">
             <img 
                 src= ${img}
@@ -88,18 +95,18 @@ function renderSuggestions(suggestions){
 }
 
 //Event Listeners to approve a suggestion
-for(const suggestion of suggestions){
-    document.getElementById(suggestion.message).addEventListener("click", function (){
+for (const suggestion of suggestions) {
+    document.getElementById(suggestion.message).addEventListener("click", function () {
 
         let suggestionToApprove = suggestion.message
         let userToReward = suggestion.username
         console.log(userToReward)
-    
-        for(const suggestion of suggestions){
-            if (suggestion.message == suggestionToApprove){
-                for(const user of users){
+
+        for (const suggestion of suggestions) {
+            if (suggestion.message == suggestionToApprove) {
+                for (const user of users) {
                     //Give 50% exp to the user for a good suggestion (we want to give the players a big reward to incentivize suggestion)
-                    if (user.username == userToReward && suggestion.approval != true){
+                    if (user.username == userToReward && suggestion.approval != true) {
                         levelManager(user, suggestionReward)
                     }
                 }
@@ -115,25 +122,25 @@ for(const suggestion of suggestions){
         localStorage.setItem("suggestions", JSON.stringify(suggestions))
         localStorage.setItem("users", JSON.stringify(users))
 
-        
-})
+
+    })
 }
 
 //Event Listeners to disapprove a suggestion
-for(const suggestion of suggestions){
-    document.getElementById("downvote-" + suggestion.message).addEventListener("click", function (){
+for (const suggestion of suggestions) {
+    document.getElementById("downvote-" + suggestion.message).addEventListener("click", function () {
 
         let suggestionToApprove = suggestion.message
         let userToReward = suggestion.username
-    
-        for(const suggestion of suggestions){
-            if (suggestion.message == suggestionToApprove){
+
+        for (const suggestion of suggestions) {
+            if (suggestion.message == suggestionToApprove) {
                 //Reward User
-                for(const user of users){
+                for (const user of users) {
                     //Take exp off the user only in case the admin missclicked and approved the suggestion beforehand (we don't want to penalize the users for suggesting)
-                    if (user.username == userToReward && suggestion.approval == true){
+                    if (user.username == userToReward && suggestion.approval == true) {
                         levelManager(user, -suggestionReward)
-                    } 
+                    }
                 }
                 suggestion.approval = false
                 suggestion.disapproval = true
@@ -146,5 +153,5 @@ for(const suggestion of suggestions){
         localStorage.setItem("suggestions", JSON.stringify(suggestions))
         localStorage.setItem("users", JSON.stringify(users))
 
-})
+    })
 }
