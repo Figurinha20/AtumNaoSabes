9609c534ee78b4bdbb04f848a72da14bec4da569
Figurinha20 <figurinhapt@gmail.com>
2019-06-21 15:29:06 +0100

    Some fixes and User Manager!

--- js/models/User.js
+++ js/models/User.js
@@ -5,7 +5,7 @@ export class User {
         this.adminStat = adminStat
         this.experience = 0
         this.level = 1
-        this.profilePicture = "https://i.imgur.com/X5GQjyT.jpg"
+        this.profilePicture = "https://www.anglersjournal.com/.image/ar_16:9%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cg_faces:center%2Cq_auto:good%2Cw_768/MTQ2OTAxMTA0NjYwNzE5NDE0/yellowfin-tuna.jpg"
         this.cardCollection = ["Grande Azul"]
     }
 }
